d7934127cd60472717dd00359e441848491acdc0
Daniel Gonzalez <dgonzalez7752@gmail.com>
2019-01-24 14:14:12 -0600

    Solved all other 3 sums

--- SolveArrays/ArraySolver.cs
+++ SolveArrays/ArraySolver.cs
@@ -10,29 +10,67 @@ namespace SolveArrays
             int total = 0;
 
 			foreach (int number in numbers)
-		{
+			{
 			total = total + number;
-		}
+			}
 
 			return total;
-        }
+			}
 
         public int SumEvens(int[] numbers)
         {
-            // TODO: Implement this method
-            throw new NotImplementedException();
-        }
+            int totalOfEvens = 0;
+
+			foreach (int number in numbers)
+			{
+				if (number% 2 == 0)
+				{
+					totalOfEvens += number;
+				}
+				}
+			return totalOfEvens;
+			}
 
         public double AverageEvens(int[] numbers)
         {
-            // TODO: Implement this method
-            throw new NotImplementedException();
-        }
+            int totalOfEvens = 0;
+			int countOfEvens = 0;
+
+			foreach (int number in numbers)
+			{
+				if (number % 2 == 0)
+				{
+					totalOfEvens += number;
+					countOfEvens += 1;
+				}
+			}
+
+			if (countOfEvens == 0)
+			{
+				return 0;
+			}
+			else
+			{
+				double average = (double)totalOfEvens / (double)countOfEvens;
+
+				return average;
+			}
+		}
+		
 
         public bool IsSumOdd(List<int> numbers)
         {
-            // TODO: Implement this method
-            throw new NotImplementedException();
+			//get the sum of all the numbers in the list
+			//list needs to be converted to an array
+            int total = Sum(numbers.ToArray());
+
+			// check if the total is odd. Return true if it is, false if not
+			if (total % 2 != 0)
+		{
+				return true;
+		}
+
+				return false;
         }
     }
 }
